c3512efa128284f76891eb9e99127ce517c39dd4
RollandV <rolland.vaillancourt@cgi.com>
2020-08-13 11:30:22 +0000

    Small minor fixes

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 class Main { // Bureau de drummondville
     public static void main(String[] args) {
+        
         CGIManagement dominic = new CGIManagement();
 
-        Employee simon = new Employee("Simon Villiard");
+        Employee simon = new Developer("Simon Villiard");
         dominic.whipEmployee(simon);
 
-        Employee samuel = new Employee("Samuel Dongmo Tedontsa");
+        Employee samuel = new QA("Samuel Dongmo Tedontsa");
         dominic.whipEmployee(samuel);
-  }
+    }
 }
--- QA.java
+++ QA.java
@@ -1,6 +1,6 @@
 public class QA extends Employee {
     public QA(String name) {
         super(name);
-        System.out.println("QA is ready to assurance quality !");
+        System.out.println("QA is ready to assure quality !");
     }
 }
